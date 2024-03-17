9c71713ccb63e41fb7ee8251526102391e45b5e2
TEspelien <39291801+TEspelien@users.noreply.github.com>
2024-03-17 16:12:58 -0400

    Initial data gathering, EDA, and gradient descent optimizer
    neural network optimizer does not work yet

---
+++ eda.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import math
+
+weekly_returns = pd.read_csv('weekly_returns.csv')
+
+tickers = weekly_returns.columns
+
+#find the mean of each column of weekly returns
+mu_is = pd.DataFrame(weekly_returns.apply(lambda col: col.mean()))
+
+#calculate the covariance of each pair i, j of tickers
+#note that cov(i, i) is simply its variance
+variances = pd.DataFrame([[weekly_returns.iloc[:, i].cov(weekly_returns.iloc[:, j]) \
+             for j in range(len(tickers))] for i in range(len(tickers))])
+
+print(mu_is)
+
+variances.style.format("{:.5f}")
+
+mu_is.to_csv('mu_is.csv', index = False)
+
+variances.to_csv('variances.csv', index = False)
---
+++ get_data.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import math
+import yfinance as yf
+
+tickers =["XLC", "XLP", "XLY", "XLE", "XLF", "XLV", "XLI", "XLB", "XLRE", "XLK", "XLU"]
+
+df = yf.download(tickers, interval = '1wk', start = '2023-01-01', end = '2023-12-31', group_by = 'ticker')
+
+#the download seems to change the order of tickers so resort before continuing
+sorted_columns = [t for t in tickers if t in df.columns]
+
+df = df.loc[:,sorted_columns]
+
+#each ticker has 6 columns, only keep 0 & 4
+#drop 1, 2, 3, 5 for each ticker
+
+to_drop = [k for k in range(6 * len(tickers)) if k%6 not in [0, 4]]
+
+df = df.drop(df.columns[to_drop], axis=1)  # df.columns is zero-based pd.Index
+
+print(df.head())
+
+df.to_csv('open_close.csv', index=False)
+
+
+#calculate weekly returns
+
+def calc_returns(row):
+
+    returns = []
+
+    for ticker_idx in range(len(tickers)):
+        open_ = row[ticker_idx * 2]
+        close = row[ticker_idx * 2 + 1]
+        returns.append((close - open_) / open_)
+
+    return returns
+
+weekly_returns = pd.DataFrame(df.apply(calc_returns, axis=1).tolist(), columns=tickers)
+
+print(weekly_returns.head())
+
+weekly_returns.to_csv('weekly_returns.csv', index = False)
+
+
+
