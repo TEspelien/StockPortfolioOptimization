eb5ae1b7f04bf8020c915074fc514c6ec2e9895d
TEspelien <trajanespelien@gmail.com>
2024-03-19 14:24:43 -0400

    updated initial_guess calculations

--- gd_optimizer.py
+++ gd_optimizer.py
@@ -29,35 +29,66 @@ def weekly_return(W):
 def constraint(W):
 
     # W * mu_is = mu
-    return 100 * (weekly_return(W) - mu)**2
+    return 
 
-# the function we are trying to optimize is sigma_sq(W) with a penalty given by constraint(W, mu)
+# function to optimize:
+# sigma_sq(W) with penalties
+# regularize weights
+# sum(W) as close to 1
+# weekly returns as close to target
 
-def to_minimize(W):
-    W = W * 1/sum(W)
-    return sigma_sq(W) + constraint(W)
+def custom_loss(W):
+    return sigma_sq(W) + sum(W**2) + 10 * (1 - sum(W))**2 + 100 * (weekly_return(W) - mu)**2
 
-initial_guess = tf.Variable(initial_value = np.array([1/n for _ in range(n)]), trainable=True) 
+#pick many different initial points and optimize from each, then pick the best
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.1)
+minima = []
 
+num_attempts = 20
+num_iterations = 50
 
-# Optimization loop
-num_iterations = 100
-for iter in range(num_iterations):
-    with tf.GradientTape() as tape:
-        loss = to_minimize(initial_guess)
+rng = np.random.default_rng()
+
+for attempt in range(num_attempts):
+
+    #start by randomly setting n-1 weights
+    #normal distribution parameter picker: https://www.desmos.com/calculator/jxzs8fz9qr 
+    initial_guess = rng.normal(loc = 0, scale = 0.2, size = n-1)
+
+    #change which weight is calculated last because it tends to have the highest magnitude
+
+    #calculate the value of the last weight in order to satisfy W_i * mu_i = mu
+    #w_n * mu_n + rest = mu
+    w_n = (mu - initial_guess.dot(mu_is[:-1]))/mu_is[-1]
+
+    initial_guess = np.append(initial_guess, w_n)
+
+    initial_guess = initial_guess / sum(initial_guess)
     
-    gradients = tape.gradient(loss, [initial_guess])
-    optimizer.apply_gradients(zip(gradients, [initial_guess]))
+    initial_guess = tf.Variable(initial_value = initial_guess, trainable = True)
 
-    #print progress every 10%
-    if iter % (num_iterations/10) == 0:
-        print(loss)
-        print(initial_guess)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+    # Optimization loop
+    for _ in range(num_iterations):
+        with tf.GradientTape() as tape:
+            loss = custom_loss(initial_guess)
+        
+        gradients = tape.gradient(loss, [initial_guess])
+        optimizer.apply_gradients(zip(gradients, [initial_guess]))
 
-print("Optimal solution:", initial_guess.numpy() * 100)
-print("Total allocation:", sum(initial_guess.numpy()) * 100)
-print("Minimum sigma_sq:", sigma_sq(initial_guess).numpy())
-print("Final constraint:", constraint(initial_guess)[0])
-print("Average weekly returns:", weekly_return(initial_guess)[0])
+    minima.append([initial_guess.numpy(), custom_loss(initial_guess.numpy())])
+    print(attempt, minima[-1])
+
+abs_min = minima[0][0]
+minimized = custom_loss(abs_min)
+
+for m in minima:
+    if(m[1] < minimized):
+       abs_min = m[0]
+       minimized = m[1]
+
+
+print("Optimal solution:", abs_min * 100)
+print("Total allocation:", sum(abs_min) * 100)
+print("Final loss:", minimized[0])
+print("Average weekly returns:", weekly_return(abs_min)[0])
