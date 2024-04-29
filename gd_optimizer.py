c54efa05040acc108f67c6d0ab3b6f52f9267715
TEspelien <trajanespelien@gmail.com>
2024-04-28 23:54:51 -0400

    trying to get solutions to bar graph nicely

--- gd_optimizer.py
+++ gd_optimizer.py
@@ -20,9 +20,9 @@ n = len(mu_is)
 arguments = sys.argv[1:]
 
 if len(arguments) > 0:
-    mu = float(arguments[0])
+    mus = [float(arguments[0])]
 else:
-    mu = 0.01
+    mus = [0.02, 0.025]
 
 # the variance of the function given weights W and variances previously calculated
 def sigma_sq(W):
@@ -40,7 +40,7 @@ def weekly_return(W):
 def risk_proxy(W):
     return 1e3 * sigma_sq(W)
 
-def weekly_return_penalty(W):
+def weekly_return_penalty(W, mu):
     return 1e7 * (weekly_return(W) - mu)**2
 
 def allocation_penalty(W):
@@ -48,113 +48,126 @@ def allocation_penalty(W):
     if s < 1:
         return 1e5 * (0.999-s)**2
     else:
-        return 1e6 * (s-1)**2
+        return 1e7 * (s-1)**2
 
 def regularize(W):
     return sum([1e6 * w**2 for w in W if w < 0])
 
-def custom_loss(W):
-    return risk_proxy(W) + regularize(W) + allocation_penalty(W) + weekly_return_penalty(W)
-
-#auto sorted list of portfolios, sorted by their loss
-#elements e: [[portfolio], loss]
-p1_minima = SortedKeyList(key = lambda e: e[1])
-p1_minima.add([[], 1e6])
+def custom_loss(W, mu):
+    return risk_proxy(W) + regularize(W) + allocation_penalty(W) + weekly_return_penalty(W, mu)
 
 p1_iter_counts = []
 
-p1_num_attempts = 10
+p1_num_attempts = 20
 p1_loss_change_threshold = 1e-3 #stop once loss changes by less than 0.01%
 p1_max_iterations = 200
 
 rng = np.random.default_rng()
 
-#pick many different initial points and optimize from each,
-#then pick the best local minima to explore further in phase 2
-for attempt in range(p1_num_attempts):
+solutions = []
 
-    #start by randomly setting n-1 weights
-    #normal distribution parameter picker: https://www.desmos.com/calculator/jxzs8fz9qr 
-    initial_guess = rng.normal(loc = 0.2, scale = 0.15, size = n-1)
+for mu in mus:
 
-    #calculate the value of the last weight in order to satisfy W_i * mu_i = mu
-    #w_n * mu_n + rest = mu
-
-    w_n = (mu - initial_guess.dot(mu_is[:-1]))/mu_is[-1]
-
-    initial_guess = np.append(initial_guess, w_n)
-
-    #for the bonus, shorting is not allowed so if the mu < 0 we don't want that stock
-    initial_guess[negative_mus] = 0
-
-    initial_guess = tf.Variable(initial_value = initial_guess, trainable = True)
-
-    optimizer = tf.keras.optimizers.SGD(learning_rate=0.00005, momentum = 0.8, clipvalue = 1.0)
+    #auto sorted list of portfolios, sorted by their loss
+    #elements e: [[portfolio], loss]
+    p1_minima = SortedKeyList(key = lambda e: e[1])
+    p1_minima.add([[], 1e6])
     
-    prev_loss = 1e7
-    itr = 0
+    #pick many different initial points and optimize from each,
+    #then pick the best local minima to explore further in phase 2
+    for attempt in range(p1_num_attempts):
 
-    # Optimization loop
-    
-    while itr < p1_max_iterations:
+        #start by randomly setting n-1 weights
+        #normal distribution parameter picker: https://www.desmos.com/calculator/jxzs8fz9qr 
+        initial_guess = rng.normal(loc = 0.2, scale = 0.15, size = n-1)
 
-        with tf.GradientTape() as tape:
-            loss = custom_loss(initial_guess)
+        #calculate the value of the last weight in order to satisfy W_i * mu_i = mu
+        #w_n * mu_n + rest = mu
+
+        w_n = (mu - initial_guess.dot(mu_is[:-1]))/mu_is[-1]
+
+        initial_guess = np.append(initial_guess, w_n)
+
+        #for the bonus, shorting is not allowed so if the mu < 0 we don't want that stock
+        initial_guess[negative_mus] = 0
+
+        initial_guess = tf.Variable(initial_value = initial_guess, trainable = True)
+
+        optimizer = tf.keras.optimizers.SGD(learning_rate=0.00005, momentum = 0.8, clipvalue = 1.0)
         
-        gradients = tape.gradient(loss, [initial_guess])
-        optimizer.apply_gradients(zip(gradients, [initial_guess]))
+        prev_loss = 1e7
+        itr = 0
 
-        loss = loss.numpy()[0]
+        # Optimization loop
+        
+        while itr < p1_max_iterations:
 
-        loss_change = (loss - prev_loss) / prev_loss
+            with tf.GradientTape() as tape:
+                loss = custom_loss(initial_guess, mu)
+            
+            gradients = tape.gradient(loss, [initial_guess])
+            optimizer.apply_gradients(zip(gradients, [initial_guess]))
 
-        #print(itr, loss, loss_change)
+            loss = loss.numpy()[0]
 
-        prev_loss = loss
+            loss_change = (loss - prev_loss) / prev_loss
 
-        #early stopping: if loss is not good enough by a certain point, dont waste time on this attempt
+            #print(itr, loss, loss_change)
 
-        if itr == 60 and loss > 1000 * p1_minima[0][1]:
-            break
+            prev_loss = loss
 
-        if itr == 120 and loss > 100 * p1_minima[0][1]:
-            break
+            #early stopping: if loss is not good enough by a certain point, dont waste time on this attempt
 
-        if itr >= 180 and abs(loss_change) > p1_loss_change_threshold:
-            break;
+            if itr == 60 and loss > 1000 * p1_minima[0][1]:
+                break
 
-        itr += 1;
+            if itr == 120 and loss > 100 * p1_minima[0][1]:
+                break
 
+            if itr >= 180 and abs(loss_change) > p1_loss_change_threshold:
+                break;
 
-    p1_minima.add([initial_guess.numpy(), prev_loss])
-
-    
-    p1_iter_counts.append(itr)
-    print("attempt ", attempt, "loss: ", prev_loss, "itr: ", itr)
-
-#focus on the best 10% of local minima found in phase 1
-
-p1_minima = p1_minima[:int(p1_num_attempts * 0.1)]
-
-#print([e[1] for e in p1_minima])
-
-p1_minima_df = pd.DataFrame(data = [np.append(e[0], e[1]) for e in p1_minima], columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'loss'])
-
-p1_minima_df.to_csv('p1_minima.csv', index = False)
-
-optimal = p1_minima[0][0]
+            itr += 1;
 
 
-print("Optimal solution:", optimal * 100)
-print("Total allocation:", sum(optimal) * 100)
-print("Final loss:", p1_minima[0][1])
-print("Risk proxy:", risk_proxy(optimal))
-print("Regularization penalty:", regularize(optimal))
-print("Allocation penalty:", allocation_penalty(optimal))
-print("Weekly return penalty:", weekly_return_penalty(optimal))
-print("Average weekly returns:", weekly_return(optimal))
-print("Requested weekly return:", mu)
+        p1_minima.add([initial_guess.numpy(), prev_loss])
 
-print("Phase 1 statistics:")
-print("Mean iterations used:", mean(p1_iter_counts))
-print("Attempts with max iterations used:", p1_iter_counts.count(p1_max_iterations))
+        
+        p1_iter_counts.append(itr)
+        print("attempt ", attempt, "loss: ", prev_loss, "itr: ", itr)
+
+    #focus on the best 10% of local minima found in phase 1
+
+    #p1_minima = p1_minima[:int(p1_num_attempts * 0.1)]
+
+    #print([e[1] for e in p1_minima])
+
+    #p1_minima_df = pd.DataFrame(data = [np.append(e[0], e[1]) for e in p1_minima], columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'loss'])
+
+    #p1_minima_df.to_csv('p1_minima.csv', index = False)
+
+    optimal = p1_minima[0][0]
+
+    solution = [mu] + [100 * x for x in optimal]
+
+    print(solution)
+
+    solutions.append(solution)
+
+
+    print("Optimal solution:", optimal * 100)
+    print("Total allocation:", sum(optimal) * 100)
+    print("Final loss:", p1_minima[0][1])
+    print("Risk proxy:", risk_proxy(optimal))
+    print("Regularization penalty:", regularize(optimal))
+    print("Allocation penalty:", allocation_penalty(optimal))
+    print("Weekly return penalty:", weekly_return_penalty(optimal, mu))
+    print("Average weekly returns:", weekly_return(optimal))
+    print("Requested weekly return:", mu)
+
+
+for s in solutions:
+    print(s)
+
+solutions_df = pd.DataFrame(data = solutions, columns = ['mu', 'x1', 'x2', 'x3', 'x4', 'x5'])
+solutions_df.to_csv('solutions.csv', index = False)
